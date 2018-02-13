9c41bd298fb564d9c9daff5d67c9e8e2c7d168a6
constairs <constairs@gmail.com>
2018-02-14 01:08:34 +0700

    Create App, News & Commetns components

--- js/app.js
+++ js/app.js
@@ -1,4 +1,47 @@
+let photos = ['images/1.jpg','images/2.jpg', 'images/3.jpg', 'images/4.jpg'];
+
+// ReactDOM.render(
+//     <App>
+//         <Photos photos=photos />
+//         <lastNews />
+//         <Comments />
+//     </App>,
+//     document.getElementById('root')
+// );
+
+let News = React.createClass({
+    render: function() {
+        return (
+            <div className="news">
+                К Сожалению новостей нет.
+            </div>
+        );
+    }
+});
+
+let Comments = React.createClass({
+    render: function() {
+        return (
+            <div className="comments">
+                Комментариев пока нет.
+            </div>
+        );
+    }
+});
+
+let App = React.createClass({
+    render: function() {
+        return (
+            <div className="app">
+                App component
+                <News />
+                <Comments />
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    React.createElement('h1', null, 'Start'),
-    document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );

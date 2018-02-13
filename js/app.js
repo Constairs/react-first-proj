d197890947a316c12d29e4eb60fcea9a66438b18
constairs <constairs@gmail.com>
2018-02-14 01:50:31 +0700

    Add prop data to News

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 let photos = ['images/1.jpg','images/2.jpg', 'images/3.jpg', 'images/4.jpg'];
 
+let myNews = [
+    {
+        author: 'Саша Печкин',
+        text: 'В четверг, четвертого числа...'
+    },
+    {
+        author: 'Просто Вася',
+        text: 'Считаю, что $ должен стоить 1488 рублей!'
+    },
+    {
+        author: 'Гость',
+        text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    }
+];
+
 // ReactDOM.render(
 //     <App>
 //         <Photos photos=photos />
@@ -10,17 +25,27 @@ let photos = ['images/1.jpg','images/2.jpg', 'images/3.jpg', 'images/4.jpg'];
 // );
 
 let News = React.createClass({
-    render: function() {
+    render: function () {
+        let data = this.props.data;
+        let newsTemplate = data.map((item, index) => {
+            return (
+                <div key={index}>
+                    <p className="news-author">{item.author}:</p>
+                    <p className="news-text">{item.text}</p>
+                </div>
+            )
+        });
+
         return (
             <div className="news">
-                К Сожалению новостей нет.
+                {newsTemplate}
             </div>
         );
     }
 });
 
 let Comments = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="comments">
                 Комментариев пока нет.
@@ -30,11 +55,11 @@ let Comments = React.createClass({
 });
 
 let App = React.createClass({
-    render: function() {
+    render: function () {
         return (
             <div className="app">
                 App component
-                <News />
+                <News data={myNews} /> {/*add data prop*/}
                 <Comments />
             </div>
         );
